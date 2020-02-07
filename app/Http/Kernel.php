3e8e3599f20ede3c051679f7e490b0bee53041ed
Angger P. Putro @(HP :-- Ubuntu) <anggerpputro@gmail.com>
2020-02-07 14:04:59 +0700

    install gemboot gateway

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \Barryvdh\Cors\HandleCors::class,
         // \App\Http\Middleware\CheckPermission::class,
         \App\Http\Middleware\HandleFormData::class,
+        \Gemboot\Gateway\Middleware\CheckToken::class,
     ];
 
     /**
